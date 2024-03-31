52b325704812a795659ff42c87446716b1216175
ojy9612 <ojy9612@gmail.com>
2024-04-01 00:06:01 +0900

    Webhook 기초 작업 완

--- src/main/kotlin/com/zeki/kisvolkotlin/exception/ResponseCode.kt
+++ src/main/kotlin/com/zeki/kisvolkotlin/exception/ResponseCode.kt
@@ -34,4 +34,5 @@ enum class ResponseCode(
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500_0", "서버측 에러"),
     INTERNAL_SERVER_WEBCLIENT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500_1", "WebClient 통신 에러"),
     INVALID_PROFILE(HttpStatus.INTERNAL_SERVER_ERROR, "500_2", "유효하지 않은 TradeMode 입니다."),
+    GOOGLE_CHAT_CONNECT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500_3", "Google Chat 연동 에러"),
 }
