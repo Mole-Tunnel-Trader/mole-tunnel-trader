bc3b1955a53d491d3e108c7139a92ec8226e3246
ojy9612 <ojy9612@gmail.com>
2025-03-19 12:14:01 +0900

    새 SchedulerController 추가 및 기존 서비스 로직 개선
    - SchedulerController 생성 및 스케줄러 관련 endpoint 추가
    - retrieveTokenFromKis 메서드에 grantType 파라미터 추가
    - Dockerfile에 OOM 발생 시 힙 덤프 설정 적용

---
+++ kis-server/src/main/kotlin/com/zeki/kisserver/domain/schduler/SchedulerController.kt
@@ -0,0 +1,28 @@
+package com.zeki.kisserver.domain.schduler
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/scheduler")
+class SchedulerController(
+    private val scheduler: Scheduler
+) {
+
+    @GetMapping("/data-go")
+    fun getDataGo() {
+        scheduler.updateHolidayAndStockCode()
+    }
+    
+    @GetMapping("/stock-info")
+    fun getStockInfo() {
+        scheduler.updateStockInfo()
+    }
+
+    @GetMapping("/stock-price")
+    fun getStockPrice() {
+        scheduler.updateStockPrice()
+    }
+
+}
